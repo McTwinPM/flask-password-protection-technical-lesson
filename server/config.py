3587a66d79850dad4a57e0d9ac2a0713343f34f6
Michael McCormack <mcctwin@gmail.com>
2026-01-08 21:17:02 -0600

    final solution

--- server/config.py
+++ server/config.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -12,5 +13,8 @@ app.json.compact = False
 db = SQLAlchemy()
 migrate = Migrate(app, db)
 db.init_app(app)
+
+# Create bcrypt instance from app
+bcrypt = Bcrypt(app)
 
 api = Api(app)
